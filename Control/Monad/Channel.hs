9d8e24fd34d2821a4d92c835d0cf0838ad0b35d0
Alexander Altman <alexanderaltman@me.com>
2013-02-22 21:22:50 -0800

    Setup
    [none]

---
+++ Control/Monad/Channel.hs
@@ -0,0 +1,8 @@
+module Control.Monad.Channel (module Control.Monad.Trans.Channel,
+                              module Control.Monad.Channel.Class,
+                              module Control.Monad.Trans,
+                              module Control.Monad.Identity) where
+import Control.Monad.Trans.Channel hiding ()
+import Control.Monad.Channel.Class
+import Control.Monad.Trans
+import Control.Monad.Identity
