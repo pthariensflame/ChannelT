be1af342c0330949e19d5426f2cf5defb05c93eb
Alexander Altman <alexanderaltman@me.com>
2013-02-25 20:17:06 -0800

    `runWye` to `runTee`
    [none]

--- Control/Monad/Channel/Selector/Tee.hs
+++ Control/Monad/Channel/Selector/Tee.hs
@@ -84,6 +84,6 @@ FreeT a <+@< FreeT b = FreeT $ do x <- a
 runTee :: (Monad m) => TeeChannelT () () o m a -> EmptyChannelT m a
 runTee (FreeT a) = FreeT $ a >>= \x -> case x of
   Pure v -> return (Pure v)
-  Free (SyncChannel AwaitLeftTee _ iL) -> runFreeT . runWye $ il ()
-  Free (SyncChannel AwaitRightTee _ iR) -> runFreeT . runWye $ iR ()
-  Free (SyncChannel YieldTee _ iU) -> runFreeT . runWye $ iU ()
+  Free (SyncChannel AwaitLeftTee _ iL) -> runFreeT . runTee $ il ()
+  Free (SyncChannel AwaitRightTee _ iR) -> runFreeT . runTee $ iR ()
+  Free (SyncChannel YieldTee _ iU) -> runFreeT . runTee $ iU ()
