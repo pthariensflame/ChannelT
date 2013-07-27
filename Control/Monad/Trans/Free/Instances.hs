be50df64c1f86e9e89407d465b3a2501d252f9e4
Alexander Altman <alexanderaltman@me.com>
2013-07-27 02:07:47 +0000

    Change project metadata.

--- Control/Monad/Trans/Free/Instances.hs
+++ Control/Monad/Trans/Free/Instances.hs
@@ -1,9 +1,16 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, KindSignatures, ExistentialQuantification, FlexibleInstances, RankNTypes, LiberalTypeSynonyms, UndecidableInstances #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE FunctionalDependencies    #-}
+{-# LANGUAGE KindSignatures            #-}
+{-# LANGUAGE LiberalTypeSynonyms       #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE RankNTypes                #-}
+{-# LANGUAGE UndecidableInstances      #-}
 module Control.Monad.Trans.Free.Instances (FreeT(..), FreeF(..)) where
-import Control.Monad.Trans
-import Control.Monad.Base
-import Control.Monad.Morph
-import Control.Monad.Trans.Free
+import           Control.Monad.Base
+import           Control.Monad.Morph
+import           Control.Monad.Trans
+import           Control.Monad.Trans.Free
 
 instance (MonadBase b m, Functor f) => MonadBase b (FreeT f m) where
   liftBase = liftBaseDefault
