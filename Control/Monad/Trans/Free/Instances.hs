93a048d8688e46254c4731235e1387ead9685ad9
Alexander Altman <alexanderaltman@me.com>
2013-07-26 22:10:17 -0400

    Partially revert "Change project metadata."
    This partially reverts commit be50df64c1f86e9e89407d465b3a2501d252f9e4.

--- Control/Monad/Trans/Free/Instances.hs
+++ Control/Monad/Trans/Free/Instances.hs
@@ -1,16 +1,9 @@
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE FlexibleInstances         #-}
-{-# LANGUAGE FunctionalDependencies    #-}
-{-# LANGUAGE KindSignatures            #-}
-{-# LANGUAGE LiberalTypeSynonyms       #-}
-{-# LANGUAGE MultiParamTypeClasses     #-}
-{-# LANGUAGE RankNTypes                #-}
-{-# LANGUAGE UndecidableInstances      #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, KindSignatures, ExistentialQuantification, FlexibleInstances, RankNTypes, LiberalTypeSynonyms, UndecidableInstances #-}
 module Control.Monad.Trans.Free.Instances (FreeT(..), FreeF(..)) where
-import           Control.Monad.Base
-import           Control.Monad.Morph
-import           Control.Monad.Trans
-import           Control.Monad.Trans.Free
+import Control.Monad.Trans
+import Control.Monad.Base
+import Control.Monad.Morph
+import Control.Monad.Trans.Free
 
 instance (MonadBase b m, Functor f) => MonadBase b (FreeT f m) where
   liftBase = liftBaseDefault
