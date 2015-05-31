83e08b29af0ee9397c67310425ad14c4413796d1
Alexander Altman <alexanderaltman@me.com>
2015-05-30 22:30:46 -0700

    Whoops, forgot something!

--- src/Control/Monad/Trans/Free/Instances.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
-module Control.Monad.Trans.Free.Instances (FreeT,
-                                           module Control.Monad.Trans.Free) where
-import Control.Monad.Trans
-import Control.Monad.Base
-import Control.Monad.Morph
-import Control.Monad.Trans.Free
