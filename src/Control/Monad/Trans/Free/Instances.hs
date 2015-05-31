7ebe59ab82919029075c6730658f58bf8fed8160
Alexander Altman <alexanderaltman@me.com>
2015-05-30 22:30:06 -0700

    rearrangements and revampings

--- src/Control/Monad/Trans/Free/Instances.hs
+++ src/Control/Monad/Trans/Free/Instances.hs
@@ -5,9 +5,3 @@ import Control.Monad.Trans
 import Control.Monad.Base
 import Control.Monad.Morph
 import Control.Monad.Trans.Free
-
-instance (MonadBase b m, Functor f) => MonadBase b (FreeT f m) where
-  liftBase = liftBaseDefault
-
-instance (Functor f) => MFunctor (FreeT f) where
-  hoist = hoistFreeT
